e023c155c74409e54149ef3daab42ea60993ceff
syamimhazmi <syamimhazmi@gmail.com>
2025-07-28 15:39:22 +0700

    added 3 more tests

--- src/maze_solver.rs
+++ src/maze_solver.rs
@@ -73,11 +73,11 @@ fn walk(
     false
 }
 
-fn solver(maze: Vec<String>, wall: char, start: Point, end: Point) -> Vec<Point> {
+fn solver(maze: &Vec<String>, wall: char, start: Point, end: Point) -> Vec<Point> {
     let mut seen = vec![vec![false; maze[0].len()]; maze.len()];
     let mut path = Vec::new();
 
-    walk(&maze, wall, start, end, &mut seen, &mut path);
+    walk(maze, wall, start, end, &mut seen, &mut path);
 
     path
 }
@@ -101,7 +101,7 @@ mod tests {
         let start = Point::new(10, 0);
         let end = Point::new(1, 5);
 
-        let path = solver(maze, wall, start, end);
+        let path = solver(&maze, wall, start, end);
 
         assert!(!path.is_empty(), "Should find a path");
         assert_eq!(path[0], start, "Path should start at starting point");
@@ -116,8 +116,85 @@ mod tests {
         let start = Point::new(1, 1);
         let end = Point::new(0, 0);
 
-        let path = solver(maze, wall, start, end);
+        let path = solver(&maze, wall, start, end);
 
         assert!(path.is_empty(), "Should not find any path when blocked");
+    }
+
+    #[test]
+    fn test_start_equals_end() {
+        let maze = vec![
+            "xxxxx".to_string(),
+            "x   x".to_string(),
+            "x   x".to_string(),
+            "x   x".to_string(),
+            "xxxxx".to_string(),
+        ];
+
+        let wall = 'x';
+        let start = Point::new(2, 2);
+        let end = Point::new(2, 2);
+
+        let path = solver(&maze, wall, start, end);
+
+        assert_eq!(path.len(), 1, "Path should contain exactly one point");
+        assert_eq!(path[0], start, "Path should contain the start/end point");
+    }
+
+    #[test]
+    fn test_complex_maze_multiple_paths() {
+        let maze = vec![
+            "xxxxxxxxxxxxxxxxx".to_string(),
+            "x     x         x".to_string(),
+            "x xxx x xxxxxxx x".to_string(),
+            "x   x x x     x x".to_string(),
+            "xxx x x x xxx x x".to_string(),
+            "x   x   x   x   x".to_string(),
+            "x xxxxxxx xxx xxx".to_string(),
+            "x               x".to_string(),
+            "xxxxxxxxxxxxxxxxx".to_string(),
+        ];
+
+        let wall = 'x';
+        let start = Point::new(1, 1);
+        let end = Point::new(15, 7);
+
+        let path = solver(&maze, wall, start, end);
+
+        assert!(!path.is_empty(), "Should find a path complex maze");
+        assert_eq!(path[0], start, "Path should start at start point");
+        assert_eq!(path[path.len() - 1], end, "Path should end at end point");
+
+        // verify path doesn't go through walls
+        for point in &path {
+            let ch = maze[point.y].chars().nth(point.x).unwrap();
+
+            assert_ne!(ch, wall, "Path should not go through wall");
+        }
+    }
+
+    #[test]
+    fn test_edge_boundries() {
+        let maze = vec![
+            "     ".to_string(),
+            "     ".to_string(),
+            "     ".to_string(),
+        ];
+
+        let wall = 'x';
+        let start = Point::new(0, 0);
+        let end = Point::new(4, 2);
+
+        let path = solver(&maze, wall, start, end);
+
+        assert!(!path.is_empty(), "Should find path from corner to corner");
+        assert_eq!(path[0], start, "Path should start at top-left");
+        assert_eq!(path[path.len() - 1], end, "Path should end at bottom-right");
+
+        // Should find a valid path (could be many valid solutions)
+        assert!(
+            path.len() >= 3,
+            "Path should have reasonable length for this distance"
+        );
     }
 }
